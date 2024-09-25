d61334b40590449f75f9edb31ceb3e09c4948c49
Anatolii Sokolovskyi <sans357@gmail.com>
2024-09-25 22:40:35 +0300

    hw2 v2

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,9 @@
-function formatMessage(message, maxLength) {}
+function formatMessage(message, maxLength) {
+    if (message.length > maxLength){
+return message.slice(0, maxLength) + "...";
+    }
+return message;
+}
 console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
 console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
 console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
